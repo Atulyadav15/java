9d465ef770a091a6e2f59b3267eb5194497db52d
atulyadav15 <aty15112003@gmail.com>
2026-08-18 12:30:10 +0530

    Initial commit

---
+++ firstclass.java
@@ -0,0 +1,26 @@
+public class firstclass {
+    public static void main(String[] args)  {
+        // System.out.print("hello world"); the both line print the same line 
+        // System.out.print("hello world");
+
+        // to print line by line we use println
+
+
+        // System.out.println("hello world");
+        // System.out.println("hello world");
+
+        // // to print in diff diff line we use /n
+
+        // System.out.print("hello world\n");
+        // System.out.print("hello world");
+
+
+        //  print the pattern
+       System.out.println("*");
+       System.out.println("**");
+       System.out.println("***");
+       System.out.println("****");
+
+
+    }
+}
